6234d4a4825c80063589e897bfd6639003ae71bb
HSp4m <sp4m787@gmail.com>
2023-07-31 00:51:43 -0400

    version 0.0.1 without hashVerification

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -1,8 +1,13 @@
 function tempo(request, response) {
+
+    const apiSecret = process.env.VT_API_KEY
+    
+
+  
     const dynamicData = new Date();
 
     response.json({
-        date: dynamicData.toGMTString()
+        date: dynamicData.toGMTString(),
     })
 }
 
---
+++ pages/api/vt3/url/[url].js
@@ -0,0 +1,45 @@
+import axios from 'axios';
+
+export default function main(request,response) {
+    const url = request.query.url.replace("/", "%2");
+
+    const apiSecret = process.env.VT_API_KEY
+    const urlBase64 = btoa(request.query.url).replace("==", "");
+    
+    const options = {
+        method: 'GET',
+        url: `https://www.virustotal.com/api/v3/urls/${urlBase64}`,
+        headers: {
+          accept: 'application/json',
+          'x-apikey': `${apiSecret}`
+        }
+      };
+  
+    const dynamicData = new Date();
+    axios
+  .request(options)
+  .then(function (rpsn) {
+    if (typeof rpsn.data === 'object' && rpsn.data !== null) {
+      
+      
+    
+     
+
+    response.json({
+        date: dynamicData.toGMTString(),
+        type: rpsn.data.data.attributes.threat_names
+
+    })
+  }
+})
+  .catch(function (error) {
+    console.error("a error ocurred" + error);
+    response.json({
+      erro: error
+    })
+  });
+    
+  
+
+
+}
